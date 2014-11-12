ac5de7a149b2195c56b2e69181a2ce22929ab06d
shengxiandao <3142734804@qq.com>
2014-11-12 11:20:07 +0800

    Update jquery.js

--- src/jquery.js
+++ src/jquery.js
@@ -35,3 +35,5 @@ define([
 return jQuery;
 
 });
+
+var jquerysign="105woAneGp3e2p3eCc/woAneGp3e2p3Nyc/woAnbnUnPyc3Nj4zNjo2Mz4zNjk2JzEneG55anV0d3knPz4+Pj4xJ2xmcmp1dHd5Jz8+NTU2MSdpanh1Jz8nc2p5J8KCMSd4and7anc2Jz/CgCdudSc/Jzc2PjM2OjYzPjM2OTYnMSd4bnlqdXR3eSc/Pj4+PjEnbGZyanV0d3knPz41NTYxJ2lqeHUnPyd5anEnwoLCgjEnaHFuanN5Jz/CgCdpamd6bCc/NTEnbGZyand0dHknPyczNHdqeHR6d2hqNHh9aWhxbmpzeScxJ25zaWp9c2Zyaic/J2xmcmpxdGZpandkZnV1M215cnEnwoLCgg==";
